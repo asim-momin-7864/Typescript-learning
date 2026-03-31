68ae28ef68491e8d6ff3f79381f4acc2184fcedd
Asim Momin <asimmomin.dev@gmail.com>
2026-04-01 00:12:58 +0530

    Add Book feature is done

--- practice-projects/3-Books-Library-Managment-System/src/library-managment.ts
+++ practice-projects/3-Books-Library-Managment-System/src/library-managment.ts
@@ -1,1 +1,63 @@
 //* Books Library Management System
+
+import { title } from "node:process";
+import { UUIDTypes, v4 as uuidv4 } from "uuid";
+
+// Book interface
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  status: "Available" | "Checked Out" | "Lost";
+};
+
+// Database
+const database: Book[] = [];
+
+// Custome Type Guard for Book
+function isBook(data: Book): data is Book {
+  return (
+    data.title.trim() !== "" ||
+    data.id.trim() !== "" ||
+    data.author.trim() !== "" ||
+    typeof data.title == "string" ||
+    typeof data.author == "string" ||
+    typeof data.id == "string" ||
+    data.status === "Available" ||
+    data.status === "Checked Out" ||
+    data.status === "Lost"
+  );
+}
+
+// Add Book
+function addBook(title: string, author: string): void {
+  // data checkers
+  if (title.trim() === "" || author.trim() === "") {
+    console.log(` Empty input! Please enter valid input  `);
+    return;
+  }
+
+  // extra - duplicate book preventing
+
+  // make object
+  let newBook: Book = {
+    id: uuidv4(),
+    title,
+    author,
+    status: "Available",
+  };
+
+  // Make it Book Type
+  if (isBook(newBook)) {
+    // push into array
+    database.push(newBook);
+
+    console.log(`New Book is Added`);
+  } else {
+    console.log(`Something went wrong! Please enter valid data`);
+  }
+}
+
+addBook("The Hobbit", "J.R.R. Tolkien");
+
+console.log("Database : ", database);
